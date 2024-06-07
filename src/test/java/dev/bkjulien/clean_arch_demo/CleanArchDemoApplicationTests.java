5b083b022461ff9c74347f35183967af6b895905
optimiste-hazel <optimiste.hazel@gmail.com>
2024-06-07 08:38:54 +0000

    commit test

--- src/test/java/dev/bkjulien/clean_arch_demo/CleanArchDemoApplicationTests.java
+++ src/test/java/dev/bkjulien/clean_arch_demo/CleanArchDemoApplicationTests.java
@@ -1,7 +1,12 @@
 package dev.bkjulien.clean_arch_demo;
 
+import dev.bkjulien.clean_arch_demo.core.application.usecases.CreateUserInteractor;
+import dev.bkjulien.clean_arch_demo.core.domain.entity.Utilisateur;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CleanArchDemoApplicationTests {
@@ -9,5 +14,6 @@ class CleanArchDemoApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
 
 }
